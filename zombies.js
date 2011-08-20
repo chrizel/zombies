836bb40f01e24b5d6da08cc00e761ac7b44d755d
Christian Zeller <chrizel@gmail.com>
2011-08-20 14:01:11 +0200

    added shooting

--- zombies.js
+++ zombies.js
@@ -23,17 +23,26 @@ var InputHandler = Backbone.Model.extend({
 var Object = Backbone.Model.extend({
 
     initialize: function(x, y, width, height) {
+        this.width = width;
+        this.height = height;
         this.div = $('<div>')
+                      .addClass('object')
                       .css({
                           'background-image': 'url(res.png)',
                           'background-position': '-' + x + 'px -' + y + 'px'
                       })
                       .width(width)
                       .height(height)
-                      .appendTo('body');
+                      .appendTo('#screen');
         this.position(0, 0);
         this.rotation(0);
+
+        game.addObject(this);
     },   
+
+    setSprite: function(x, y) {
+        $(this.div).css('background-position', '-' + x + 'px -' + y + 'px');
+    },
 
     position: function(x, y) {
         this.x = x;
@@ -58,6 +67,20 @@ var Object = Backbone.Model.extend({
         this.position(this.x-xOffset, this.y+yOffset);
     },
 
+    collider: function() {
+        var self = this;
+        var result = null;
+        _.each(game.objects, function(object) {
+            if (object != self &&
+                self.x >= object.x && self.x <= object.x+object.width &&
+                self.y >= object.y && self.y <= object.y+object.height)
+            {
+                result = object;
+            }
+        });
+        return result;
+    },
+
     frame: function() {
     }
 
@@ -66,6 +89,7 @@ var Object = Backbone.Model.extend({
 var Zombie = Object.extend({
     initialize: function() {
         Object.prototype.initialize.call(this, 48, 0, 48, 32);
+        this.health = 100;
     },
 
     frame: function() {
@@ -77,7 +101,37 @@ var Zombie = Object.extend({
         var alpha = Math.acos((a*a-b*b-c*c) / (-2*b*c)) * (a > 0 ? -1 : 1);
 
         this.rotation(alpha);
-        this.go(1);
+        if (c > 16)
+            this.go(2);
+
+        this.setSprite(48, 0);
+    },
+
+    damage: function(d) {
+        this.health -= d;
+        if (this.health < 0)
+            game.removeObject(this);
+        this.setSprite(48, 32);
+    }
+});
+
+var Bullet = Object.extend({
+    initialize: function() {
+        Object.prototype.initialize.call(this, 0, 16, 5, 5);
+    },
+
+    frame: function() {
+        this.go(10);
+        if (this.x < 0 || this.y < 0 || this.x > 1024 || this.y > 768)
+            game.removeObject(this);
+
+        var collider = this.collider();
+        if (collider && collider != game.player) {
+            game.removeObject(this);
+            if (collider.damage) {
+                collider.damage(20);
+            }
+        }
     }
 });
 
@@ -86,6 +140,8 @@ var Player = Object.extend({
     KEY_RIGHT: 68,
     KEY_UP: 87,
     KEY_DOWN: 83,
+    KEY_FIRE: 32,
+    lastBullet: 0,
 
     initialize: function() {
         Object.prototype.initialize.call(this, 0, 0, 48, 16);
@@ -97,13 +153,22 @@ var Player = Object.extend({
             this.go(10);
         }
         if (this.inputHandler.pressed(this.KEY_DOWN)) {
-            this.go(-5);
+            this.go(-4);
         }
         if (this.inputHandler.pressed(this.KEY_LEFT)) {
-            this.rotation(this.rad-0.2);
+            this.rotation(this.rad-0.1);
         }
         if (this.inputHandler.pressed(this.KEY_RIGHT)) {
-            this.rotation(this.rad+0.2);
+            this.rotation(this.rad+0.1);
+        }
+        if (this.inputHandler.pressed(this.KEY_FIRE)) {
+            var now = (new Date()).getTime();
+            if (now-this.lastBullet > 200) {
+                var bullet = new Bullet();
+                bullet.position(this.x, this.y);
+                bullet.rotation(this.rad);
+                this.lastBullet = now;
+            }
         }
     }
 });
@@ -113,19 +178,18 @@ var Game = Backbone.Model.extend({
     objects: [],
 
     initialize: function() {
-        this.player = new Player();
-        this.zombie1 = new Zombie();
-        this.zombie1.position(200, 200);
-
-        this.zombie2 = new Zombie();
-        this.zombie2.position(600, 600);
-
-        this.objects.push(this.player);
-        this.objects.push(this.zombie1);
-        this.objects.push(this.zombie2);
-
         var self = this;
         window.setInterval(function() { self.frame(); }, 30);
+    },
+
+    addObject: function(object) {
+        this.objects.push(object);
+    },
+
+    removeObject: function(object) {
+        var index = _.indexOf(this.objects, object);
+        this.objects.splice(index, 1);
+        $(object.div).remove();
     },
 
     frame: function() {
@@ -135,7 +199,12 @@ var Game = Backbone.Model.extend({
     }
 });
 
-$(function() {
-    game = new Game();
-    game.player.position(300, 300);
+$(function() { 
+    game = new Game(); 
+    game.player = new Player();
+    game.player.position(500, 500);
+    (new Zombie()).position(250, 150);
+    window.setInterval(function() {
+        (new Zombie()).position(Math.random()*1024, Math.random()*786);
+    }, 3000);
 });
